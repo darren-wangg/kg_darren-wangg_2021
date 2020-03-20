a8f06391266a9314bdac702c06bdb72a6a00f346
Darren Wang <darrenwng6@gmail.com>
2020-03-19 22:26:47 -0700

    first commit

---
+++ main.java
@@ -0,0 +1,31 @@
+class Main {
+    public static void main(String[] args) {
+        // edge cases
+        if(args[0].length() != args[1].length()) {
+            System.out.println("false");
+            return;
+        }
+        // two ascii value arrays, initialize to zero
+        int map1[] = new int[256];
+        int map2[] = new int[256];
+        for(int i = 0; i < args[0].length(); i++) {
+            // mapping already exists
+            if(map2[args[1].charAt(i)] != 0) {
+                int index = map2[args[1].charAt(i)] - 1;
+                char ch = args[0].charAt(index);
+                // check if mapping consistent
+                System.out.println(map2[args[1].charAt(i)] == map1[ch]);
+                return;
+            }
+            // mapping already exists, inconsistent
+            if(map1[args[0].charAt(i)] != map2[args[1].charAt(i)]) {
+                System.out.println("false");
+                return;
+            }
+            // mapping does not exist yet, set to same value
+            map1[args[0].charAt(i)] = i + 1;
+            map2[args[1].charAt(i)] = i + 1;
+        }
+        System.out.println("true");
+    }
+}
