d9dc9dddc20494fb48c4cdec1d2e3aee3bb47c4a
Darren Wang <darrenwng6@gmail.com>
2020-04-05 21:42:51 -0700

    fixed new mapping error

--- main.java
+++ main.java
@@ -14,8 +14,9 @@ class main {
                 int index = map2[args[1].charAt(i)] - 1;
                 char ch = args[0].charAt(index);
                 // check if mapping consistent
-                System.out.println(map2[args[1].charAt(i)] == map1[ch]);
-                return;
+                if(map2[args[1].charAt(i)] != map1[ch]) {
+                    System.out.println("false");
+                }
             }
             // mapping already exists, inconsistent
             if(map1[args[0].charAt(i)] != map2[args[1].charAt(i)]) {
